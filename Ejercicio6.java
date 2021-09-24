d095f13857899fd9edb014e38618747271ad799c
Isaias TMC <isaiastufatherx@gmail.com>
2021-09-24 17:15:19 -0400

    Create Ejercicio6.java
    Division de 1 numero mediante restas usando recursividad

---
+++ Ejercicio6.java
@@ -0,0 +1,19 @@
+/**
+ * Division de un numero mediante restas.
+ * 
+ * @author Isaias
+ * @version 1.0
+ */
+public class Ejercicio6
+{
+    public int restaDeNumeros(int n, int m)
+    {
+        int res;
+        if(m > n){
+            res = 0;
+        }else{
+            res = 1 + restaDeNumeros(n - m , m);
+        }
+        return res;
+    }
+}
