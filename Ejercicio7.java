99f2fcc8fad32afef3d4f1dcbc77600f249f2ed7
Isaias TMC <isaiastufatherx@gmail.com>
2021-09-29 10:02:05 -0400

    Numeros crecientes e hibridos usando recursividad

---
+++ Ejercicio7.java
@@ -0,0 +1,23 @@
+/**
+ * Numeros Crecientes e Hibridos.
+ * 
+ * @author Isaias
+ * @version 1.0
+ */
+public class Ejercicio7
+{
+    public String esCreciente(int n){
+        String res = "";
+        int dig;
+        if(n > 0){            
+            dig = n % 10;
+            if((n / 10) % 10 > dig){
+                esCreciente(n / 10);
+                res = "Creciente";
+            }else{
+                res = "Hibrido";
+            }
+        }
+        return res;
+    }
+}
